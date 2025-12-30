54b619bc412ab119030f1984b09ad703b0803127
Aditya-Srivastava09 <adityakrsrivastava09@gmail.com>
2025-12-30 21:38:41 +0530

    Time: 0 ms (100%), Space: 47.4 MB (44.17%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,60 +1,24 @@
-class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        UnionFind uf = new UnionFind(n);
+class Solution{
+    public int findCircleNum(int[][] isConnected){
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) { // upper triangle only (matrix is symmetric)
-                if (isConnected[i][j] == 1) {
-                    uf.union(i, j);
-                }
+        int n=isConnected.length;
+        boolean[] vis=new boolean[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(isConnected,vis,i);
+                count++;
             }
         }
-
-        return uf.getCount();
-    }
-}
-
-class UnionFind {
-    private int[] parent;
-    private int[] rank;
-    private int count; // number of disjoint sets
-
-    public UnionFind(int n) {
-        parent = new int[n];
-        rank = new int[n];
-        count = n;
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-            rank[i] = 1;
-        }
-    }
-
-    public int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]); // path compression
-        }
-        return parent[x];
-    }
-
-    public void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]) {
-                parent[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {
-                parent[rootX] = rootY;
-            } else {
-                parent[rootY] = rootX;
-                rank[rootX]++;
-            }
-            count--; // two sets merged
-        }
-    }
-
-    public int getCount() {
         return count;
+    }
+
+    void dfs(int[][] g,boolean[] vis,int i){
+        vis[i]=true;
+        for(int j=0;j<g.length;j++){
+            if(g[i][j]==1&&!vis[j]){
+                dfs(g,vis,j);
+            }
+        }
     }
 }
