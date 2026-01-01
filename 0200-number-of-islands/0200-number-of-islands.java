587d96487e00933fa424c29239b6bf3130acffa1
Aditya-Srivastava09 <adityakrsrivastava09@gmail.com>
2026-01-01 12:40:10 +0530

    Time: 4 ms (48.96%), Space: 52.4 MB (31.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,44 +1,31 @@
 class Solution {
-    public static void bfs(char[][] grid, int i, int j, int row, int col, boolean[][] visited) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i, j});
-        visited[i][j] = true;
-
-        int[] n_r = {1, 0, -1, 0};
-        int[] n_c = {0, 1, 0, -1};
-
-        while (!q.isEmpty()) {
-            int[] arr = q.poll();
-            int r = arr[0];
-            int c = arr[1];
-
-            for (int k = 0; k < 4; k++) {
-                int nei_row = r + n_r[k];
-                int nei_col = c + n_c[k];
-
-                if (nei_row >= 0 && nei_row < row && nei_col >= 0 && nei_col < col
-                        && grid[nei_row][nei_col] == '1' && !visited[nei_row][nei_col]) {
-                    visited[nei_row][nei_col] = true;
-                    q.add(new int[]{nei_row, nei_col});
-                }
-            }
-        }
-    }
-
     public int numIslands(char[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        boolean[][] visited = new boolean[row][col];
-        int count = 0;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
-                    bfs(grid, i, j, row, col, visited);
-                    count++;
-                }
+       int[] dr={-1,1,0,0};
+       int[] dc={0,0,-1,1};
+       int m=grid.length;
+       int n=grid[0].length;
+       int count=0;
+       boolean[][] vis=new boolean[m][n];
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(!vis[i][j] && grid[i][j]=='1'){
+                 dfs(i,j,grid,vis);
+                 count++;
             }
         }
-        return count;
+       }
+       return count;
     }
+     void dfs(int i,int j,char[][] grid,boolean[][] vis){
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length) return;
+        if(vis[i][j]||grid[i][j]=='0') return;
+
+        vis[i][j]=true;
+
+        dfs(i+1,j,grid,vis);
+        dfs(i-1,j,grid,vis);
+        dfs(i,j+1,grid,vis);
+        dfs(i,j-1,grid,vis);
+    }
+     
 }
