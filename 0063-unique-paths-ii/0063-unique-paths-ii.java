1f62e8b9054e45d965b9b50d54e076da50909c6a
Aditya-Srivastava09 <adityakrsrivastava09@gmail.com>
2026-01-06 21:26:29 +0530

    Time: 0 ms (100%), Space: 43.5 MB (42.96%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,28 +1,27 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
-
-        if (obstacleGrid[0][0] == 1) return 0;
-        dp[0][0] = 1;
-
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = (obstacleGrid[i][0] == 0 && dp[i - 1][0] == 1) ? 1 : 0;
-        }
-
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = (obstacleGrid[0][j] == 0 && dp[0][j - 1] == 1) ? 1 : 0;
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = (obstacleGrid[i][j] == 0)
-                    ? dp[i - 1][j] + dp[i][j - 1]
-                    : 0;
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
             }
         }
-
-        return dp[m - 1][n - 1];
+        
+        return sub(m-1,n-1,dp,obstacleGrid);
+        
     }
+    public int sub(int m,int n,int[][]dp,int[][] arr){
+        
+         if(m==0 && n==0 && arr[m][n]!=1) return 1;
+         if(m<0 || n<0) return 0;
+         if(arr[m][n]==1) return 0;
+         if(dp[m][n]!=-1) return dp[m][n];
+         int left=sub(m-1,n,dp,arr);
+         int right=sub(m,n-1,dp,arr);
+         dp[m][n]=left+right;
+          return dp[m][n]; 
+    }
+   
 }
