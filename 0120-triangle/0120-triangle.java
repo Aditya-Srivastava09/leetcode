50abb3a2666d0447a4eb4d9a934a0beded3bf513
Aditya-Srivastava09 <adityakrsrivastava09@gmail.com>
2026-01-06 23:07:12 +0530

    Time: 1 ms (99.77%), Space: 45.4 MB (64.55%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int a=triangle.size();
-        int[][] dp=new int[a][a];
-        for(int i=0;i<a;i++){
-            Arrays.fill(dp[i], -1);
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
-        return sub(triangle,0,0,dp);
+        return dfs(0, 0, triangle, dp);
     }
 
-    public int sub(List<List<Integer>> triangle,int i,int j,int[][]dp){
-        if(i==triangle.size()-1){
+    private int dfs(int i, int j, List<List<Integer>> triangle, int[][] dp) {
+        if(i == triangle.size() - 1){
             return triangle.get(i).get(j);
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int a=triangle.get(i).get(j)+sub(triangle,i+1,j,dp);
-        int b=triangle.get(i).get(j)+sub(triangle,i+1,j+1,dp);
-        dp[i][j]=Math.min(a,b);
+
+        if(dp[i][j] != Integer.MAX_VALUE){
+            return dp[i][j];
+        }
+
+        int down = dfs(i + 1, j, triangle, dp);
+        int diag = dfs(i + 1, j + 1, triangle, dp);
+
+        dp[i][j] = triangle.get(i).get(j) + Math.min(down, diag);
         return dp[i][j];
     }
 }
