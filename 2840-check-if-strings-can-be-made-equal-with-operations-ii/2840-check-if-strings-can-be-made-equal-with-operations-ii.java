c7e7a531f6a33809735234e7ea208c723aac41be
Aditya-Srivastava09 <adityakrsrivastava09@gmail.com>
2026-03-31 00:29:49 +0530

    Time: 5 ms (98.68%), Space: 48 MB (65.79%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,14 +1,26 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        int[] freq = new int[52];
-        for (int i = 0; i < s1.length(); i++) {
-            int off = (i & 1) * 26;
-            freq[s1.charAt(i) - 'a' + off]++;
-            freq[s2.charAt(i) - 'a' + off]--;
-        }
-        for (int i = 0; i < 52; i++)
-            if (freq[i] != 0) return false;
-
-        return true;
+    int [] evenparity1=new int[26];
+    int [] evenparity2=new int[26];
+    int [] oddparity1=new int[26];
+    int [] oddparity2=new int[26];
+    for(int i=0;i<s1.length();i=i+2){
+        evenparity1[s1.charAt(i)-'a']++;
+    }
+     for(int i=0;i<s2.length();i=i+2){
+        evenparity2[s2.charAt(i)-'a']++;
+    }
+    for(int i=1;i<s1.length();i=i+2){
+        oddparity1[s1.charAt(i)-'a']++;
+    }
+     for(int i=1;i<s2.length();i=i+2){
+        oddparity2[s2.charAt(i)-'a']++;
+    }
+     for(int i=0;i<26;i=i+1){
+       if(evenparity1[i]!=evenparity2[i] || oddparity1[i]!=oddparity2[i]){
+        return false;
+       }
+    }
+    return true;
     }
 }
