be583e14b32db0939babc428679ad28b49ff9ffd
Aditya-Srivastava09 <adityakrsrivastava09@gmail.com>
2026-04-14 14:34:31 +0530

    Time: 11 ms (66.55%), Space: 49.2 MB (50.16%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,49 +1,42 @@
-import java.util.*;
-
-class Pair {
-    int node, time;
-    Pair(int n, int t) {
-        node = n;
-        time = t;
-    }
-}
-
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            adj.add(new ArrayList<>());
-        }
-
-        for (int[] edge : times) {
-            int u = edge[0], v = edge[1], w = edge[2];
-            adj.get(u).add(new Pair(v, w));
-        }
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.time - b.time);
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[k] = 0;
-
-        pq.add(new Pair(k, 0));
-
-        while (!pq.isEmpty()) {
-            Pair cur = pq.poll();
-            int node = cur.node, time = cur.time;
-
-            for (Pair nei : adj.get(node)) {
-                if (time + nei.time < dist[nei.node]) {
-                    dist[nei.node] = time + nei.time;
-                    pq.add(new Pair(nei.node, dist[nei.node]));
-                }
+     List<List<int[]>> a=new ArrayList<>();
+     int ni=n;
+     for(int i=0;i<ni+1;i++){
+        a.add(new ArrayList<>());
+     }
+     for(int i=0;i<times.length;i++){
+        int u=times[i][0];
+        int v=times[i][1];
+        int w=times[i][2];
+        a.get(u).add(new int[]{v,w});
+     }
+     int [] distance=new int[n+1];
+     Arrays.fill(distance,Integer.MAX_VALUE);
+     distance[k]=0;
+     PriorityQueue<int[]> pq=new PriorityQueue<>((a1,b)->a1[0]-b[0]);
+     pq.add(new int[]{0,k});
+     while(!pq.isEmpty()){
+        int[] arr=pq.poll();
+        int currweight=arr[0];
+        int currnode=arr[1];
+        for(int[] e:a.get(currnode)){
+            int nextnode=e[0];
+            int nextweight=e[1];
+            if(distance[nextnode]>currweight+nextweight){
+                  distance[nextnode]=currweight+nextweight;
+                  pq.add(new int[]{currweight+nextweight,nextnode});
+                   
             }
         }
-
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == Integer.MAX_VALUE) return -1;
-            ans = Math.max(ans, dist[i]);
+     }
+       int max =0;
+        for(int i = 1; i <= n; i++){
+            if(distance[i] == Integer.MAX_VALUE) return -1;
+            max=Math.max(max, distance[i]);
         }
-        return ans;
+
+        return max;
+
     }
 }
