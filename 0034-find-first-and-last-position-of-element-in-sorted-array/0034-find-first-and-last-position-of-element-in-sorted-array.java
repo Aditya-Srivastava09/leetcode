165890896b4daace0bfa0b65d5336afe16110d3f
Aditya-Srivastava09 <adityakrsrivastava09@gmail.com>
2026-06-21 14:56:38 +0530

    Time: 0 ms (100%), Space: 48.2 MB (30.67%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int [] result=new int[2];
-        Arrays.fill(result,-1);
+        int[] arr=new int[2];
+        Arrays.fill(arr,-1);
         int l=0;
-        int high=nums.length-1;
-        
-        while(l<=high){
-            int mid=l+(high-l)/2;
-            if(nums[mid]==target){
-                result[0]=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+        int r=nums.length-1;
+    
+        while(l<=r){
             
-        }
-         l=0;
-         high=nums.length-1;
-
-        while(l<=high){
-            int mid=l+(high-l)/2;
+            int mid=l+(r-l)/2;
             if(nums[mid]==target){
-                result[1]=mid;
-                l=mid+1;
+                 arr[0]=mid;
+                 r=mid-1;
             }
-            else if(nums[mid]<target){
-                l=mid+1;
+            else if(nums[mid]>target){
+                r=mid-1;
             }
             else{
-                high=mid-1;
+                l=mid+1;
             }
         }
-        return result;
-
+        l=0;
+        r=nums.length-1;
+        while(l<=r){
+            
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                 arr[1]=mid;
+                 l=mid+1;
+            }
+            else if(nums[mid]>target){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return arr;
     }
 }
